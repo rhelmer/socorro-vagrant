8f97ffa5dd1caf50e61c8163b5857766f5260a4f
Robert Helmer <rhelmer@rhelmer.org>
2011-05-04 17:20:26 -0700

    only create DBs and run dependent commands if needed

--- manifests/classes/socorro-db.pp
+++ manifests/classes/socorro-db.pp
@@ -19,6 +19,7 @@ class socorro-db inherits socorro-base {
     exec {
         '/usr/bin/createdb breakpad':
             require => Package['postgresql'],
+            unless => '/usr/bin/psql --list breakpad',
             alias => 'create-breakpad-db',
             user => 'postgres';
     }
@@ -26,6 +27,7 @@ class socorro-db inherits socorro-base {
     exec {
         '/usr/bin/psql -c "create role breakpad_rw login password \'aPassword\'"':
             alias => 'create-breakpad-role',
+            refreshonly => true,
             user => 'postgres',
             require => Exec['create-breakpad-db'];
     }
@@ -33,6 +35,7 @@ class socorro-db inherits socorro-base {
     exec {
         '/usr/bin/psql -c "grant all on database breakpad to breakpad_rw"':
             alias => 'grant-breakpad-access',
+            refreshonly => true,
             user => 'postgres',
             require => Exec['create-breakpad-role'];
     }
@@ -40,24 +43,28 @@ class socorro-db inherits socorro-base {
     exec {
         '/usr/bin/psql breakpad < /usr/share/postgresql/8.4/contrib/citext.sql':
             user => 'postgres',
+            refreshonly => true,
             require => [Exec['create-breakpad-db'], Package['postgresql-contrib']];
     }
 
     exec {
         '/usr/bin/psql -c "create language plpgsql" breakpad':
             user => 'postgres',
+            refreshonly => true,
             require => Exec['create-breakpad-db'];
     }
 
     exec {
         '/usr/bin/psql -c "create language plperl" breakpad':
             user => 'postgres',
+            refreshonly => true,
             require => [Exec['create-breakpad-db'], Package['postgresql-plperl']];
     }
 
     exec {
         '/usr/bin/createdb test':
             require => Package['postgresql'],
+            unless => '/usr/bin/psql --list test',
             alias => 'create-test-db',
             user => 'postgres';
     }
@@ -66,6 +73,7 @@ class socorro-db inherits socorro-base {
         '/usr/bin/psql -c "create role test login password \'aPassword\'"':
             alias => 'create-test-role',
             user => 'postgres',
+            refreshonly => true,
             require => Exec['create-test-db'];
     }
 
@@ -73,24 +81,28 @@ class socorro-db inherits socorro-base {
         '/usr/bin/psql -c "grant all on database test to test"':
             alias => 'grant-test-access',
             user => 'postgres',
+	    refreshonly => true,
             require => Exec['create-test-role'];
     }
 
     exec {
         '/usr/bin/psql test < /usr/share/postgresql/8.4/contrib/citext.sql':
             user => 'postgres',
+	    refreshonly => true,
             require => [Exec['create-test-db'], Package['postgresql-contrib']];
     }
 
     exec {
         '/usr/bin/psql -c "create language plpgsql" test':
             user => 'postgres',
+	    refreshonly => true,
             require => Exec['create-test-db'];
     }
 
     exec {
         '/usr/bin/psql -c "create language plperl" test':
             user => 'postgres',
+	    refreshonly => true,
             require => [Exec['create-test-db'], Package['postgresql-plperl']];
     }
 
