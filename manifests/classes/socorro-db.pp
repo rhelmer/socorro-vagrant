5c1011bd27c2a3da7ce60769da6e572c9d06e736
Robert Helmer <rhelmer@rhelmer.org>
2011-05-05 17:54:02 -0700

    check for specific roles/tables/langs every time

--- manifests/classes/socorro-db.pp
+++ manifests/classes/socorro-db.pp
@@ -27,6 +27,7 @@ class socorro-db inherits socorro-base {
     exec {
         '/usr/bin/psql -c "create role breakpad_rw login password \'aPassword\'"':
             alias => 'create-breakpad-role',
+            unless => '/usr/bin/psql -c "SELECT rolname from pg_roles where rolname = \'breakpad_rw\'" breakpad | grep breakpad_rw',
             user => 'postgres',
             require => Exec['create-breakpad-db'];
     }
@@ -47,6 +48,7 @@ class socorro-db inherits socorro-base {
     exec {
         '/usr/bin/psql -c "create language plpgsql" breakpad':
             user => 'postgres',
+            unless => '/usr/bin/psql -c "SELECT lanname from pg_language where lanname = \'plpgsql\'" breakpad | grep plpgsql',
             alias => 'create-language-plpgsql',
             require => Exec['create-breakpad-db'];
     }
@@ -54,6 +56,7 @@ class socorro-db inherits socorro-base {
     exec {
         '/usr/bin/psql -c "create language plperl" breakpad':
             user => 'postgres',
+            unless => '/usr/bin/psql -c "SELECT lanname from pg_language where lanname = \'plperl\'" breakpad | grep plperl',
             alias => 'create-language-plperl',
             require => [Exec['create-language-plpgsql'], Package['postgresql-plperl']];
     }
@@ -62,6 +65,7 @@ class socorro-db inherits socorro-base {
         '/usr/bin/psql -c "CREATE TABLE sessions ( session_id varchar(127) NOT NULL, last_activity integer NOT NULL, data text NOT NULL, CONSTRAINT session_id_pkey PRIMARY KEY (session_id), CONSTRAINT last_activity_check CHECK (last_activity >= 0))" breakpad':
             alias => 'create-sessions-table',
             user => 'postgres',
+            unless => '/usr/bin/psql -c "SELECT relname from pg_class where relname = \'sessions\'" breakpad | grep sessions',
             require => Exec['create-language-plperl'];
     }
 
@@ -75,6 +79,7 @@ class socorro-db inherits socorro-base {
     exec {
         '/usr/bin/psql -c "INSERT INTO productdims (product, version, branch) values (\'GenericProduct\', \'1.0\', \'1.0\')" breakpad':
             alias => 'insert-productdims',
+            unless => '/usr/bin/psql -c "select id from productdims where id = \'1\'" breakpad | grep " 1"',
             user => 'postgres',
             require => Exec['setup-schema'];
     }
@@ -82,6 +87,7 @@ class socorro-db inherits socorro-base {
     exec {
         '/usr/bin/psql -c "INSERT INTO product_visibility (productdims_id, start_date, end_date, featured) VALUES (1, \'2010-11-05\', \'2015-02-05\', true)" breakpad':
             alias => 'insert-product_visibility',
+            unless => '/usr/bin/psql -c "select productdims_id from product_visibility where productdims_id = \'1\'" breakpad | grep " 1"',
             user => 'postgres',
             require => Exec['insert-productdims'];
     }
@@ -98,6 +104,7 @@ class socorro-db inherits socorro-base {
     exec {
         '/usr/bin/psql -c "create role test login password \'aPassword\'"':
             alias => 'create-test-role',
+            unless => '/usr/bin/psql -c "SELECT rolname from pg_roles where rolname = \'test\'" test | grep test',
             user => 'postgres',
             require => Exec['create-test-db'];
     }
@@ -118,12 +125,14 @@ class socorro-db inherits socorro-base {
     exec {
         '/usr/bin/psql -c "create language plpgsql" test':
             user => 'postgres',
+            unless => '/usr/bin/psql -c "SELECT lanname from pg_language where lanname = \'plpgsql\'" test | grep plpgsql',
             require => Exec['create-test-db'];
     }
 
     exec {
         '/usr/bin/psql -c "create language plperl" test':
             user => 'postgres',
+            unless => '/usr/bin/psql -c "SELECT lanname from pg_language where lanname = \'plperl\'" test | grep plperl',
             require => [Exec['create-test-db'], Package['postgresql-plperl']];
     }
 
